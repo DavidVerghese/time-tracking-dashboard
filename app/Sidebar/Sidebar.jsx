d481f4f82ca8add0c774ee22d68cee82626136cb
David Verghese <dj.verghese@gmail.com>
2023-11-05 17:26:54 -0500

    break home into smaller components

---
+++ app/Sidebar/Sidebar.jsx
@@ -0,0 +1,9 @@
+export default function Sidebar() {
+	return (
+		<div className="bg-dark-blue sm:w-1/4 h-48 sm:h-4/5 m-4 rounded-xl">
+			<div className="bg-blue h-3/5 rounded-xl">
+			</div>
+		</div>
+	)
+}
+
--- app/page.js
+++ app/page.js
@@ -1,38 +1,13 @@
 import Image from 'next/image'
+import Grid from './Grid/Grid'
+import Sidebar from './Sidebar/Sidebar'
 
 export default function Home() {
 	return (
 		<main className="sm:flex sm:flex-row h-screen">
-		<div className="bg-dark-blue sm:w-1/4 h-48 sm:h-4/5 m-4 rounded-xl">
-			<div className="bg-blue h-3/5 rounded-xl">
-			</div>
-		</div>
-		<div className="sm:grid grid-cols-2 lg:grid-cols-3 gap-4 sm:w-3/4 sm:h-4/5 m-4">
-			<div className="bg-light-red-work rounded-xl flex flex-col justify-end max-sm:h-48">
-				<div className="bg-dark-blue h-4/5 rounded-xl">
-				</div>	
-			</div>
-			<div className="bg-soft-blue-play rounded-xl flex flex-col justify-end max-sm:h-48">
-				<div className="bg-dark-blue h-4/5 rounded-xl">
-				</div>
-			</div>
-			<div className="bg-light-red-study rounded-xl flex flex-col justify-end max-sm:h-48">
-				<div className="bg-dark-blue h-4/5 rounded-xl">
-				</div>
-			</div>
-			<div className="bg-lime-green-exercise rounded-xl flex flex-col justify-end max-sm:h-48">
-				<div className="bg-dark-blue h-4/5 rounded-xl">
-				</div>
-			</div>
-			<div className="bg-violet-social rounded-xl flex flex-col justify-end max-sm:h-48">
-				<div className="bg-dark-blue h-4/5 rounded-xl">
-				</div>
-			</div>
-			<div className="bg-soft-orange-self-care rounded-xl flex flex-col justify-end max-sm:h-48">
-				<div className="bg-dark-blue h-4/5 rounded-xl">
-				</div>
-			</div>
-		</div>
+		<Sidebar/>
+		<Grid/>
+		
 		</main>
 	)
 }
