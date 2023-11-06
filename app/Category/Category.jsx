acbface0f89fc03a6ff69bbf0b150a01ad503d41
davidjverghese <davidv@summitappliance.com>
2023-11-06 09:59:42 -0500

    Fix bug that was preventing custom colors from being loaded

--- app/Category/Category.jsx
+++ app/Category/Category.jsx
@@ -1,21 +1,21 @@
 export default function Category({ category, timeframe, key }) {
-    
-    const colorMap = { 
-		"Work": "light-red-work",
-		"Play": "soft-blue-play",
-		"Study": "light-red-study",
-		"Exercise": "lime-green-exercise ",
-		"Social": "violet-social",
-		"Self Care": "soft-orange-self-care",
+
+    const colorMap = {
+		"Work": "bg-light-red-work",
+		"Play": "bg-soft-blue-play",
+		"Study": "bg-light-red-study",
+		"Exercise": "bg-lime-green-exercise ",
+		"Social": "bg-violet-social",
+		"Self Care": "bg-soft-orange-self-care",
     }
-    
+
     return (
-        <div key={key} className={`bg-${colorMap[category.title]} rounded-xl flex flex-col justify-end max-sm:h-48`}>
+        <div key={key} className={`${colorMap[category.title]} rounded-xl flex flex-col justify-end max-sm:h-48`}>
             <div className="bg-dark-blue h-4/5 rounded-xl">
                 <h2>{category.title}</h2>
-                <p>{category.timeframes[timeframe].current}</p>
+                <p class = "text-4xl font-bold">{category.timeframes[timeframe].current}</p>
                 <p>Last: {category.timeframes[timeframe].previous}</p>
-			</div>	
+			</div>
 		</div>
     )
 }
