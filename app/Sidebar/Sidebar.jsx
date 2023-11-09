90b787086fdf923b54add7a4e004597858507acf
David Verghese <dj.verghese@gmail.com>
2023-11-08 23:32:53 -0500

    add in pictures and icons

--- app/Sidebar/Sidebar.jsx
+++ app/Sidebar/Sidebar.jsx
@@ -1,7 +1,9 @@
+
 export default function Sidebar() {
 	return (
 		<div className="bg-dark-blue sm:w-1/4 h-48 sm:h-4/5 m-4 rounded-xl">
-			<div className="bg-blue h-3/5 rounded-xl">
+      <div className="bg-blue h-3/5 rounded-xl">
+        <img src="/images/image-jeremy.png"/>
 			</div>
 			<div className="flex-col">
 				<p>Daily</p>
