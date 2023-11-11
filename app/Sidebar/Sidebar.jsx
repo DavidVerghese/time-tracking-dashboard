d4a05716b8a3566eb68d252a6a1adb2288764db4
David Verghese <dj.verghese@gmail.com>
2023-11-11 14:19:07 -0500

    get link color to change

--- app/Sidebar/Sidebar.jsx
+++ app/Sidebar/Sidebar.jsx
@@ -1,26 +1,39 @@
-import Link from 'next/link'
-import Image from 'next/image'
+"use client";
+import Link from "next/link";
+import Image from "next/image";
+import { usePathname } from "next/navigation";
 
 export default function Sidebar() {
+	const pathname = usePathname();
+	function linkColor(timeframe) {
+		return pathname == timeframe ? `text-pale-blue font-semibold` : `text-desaturated-blue`
+	}
 	return (
-		<div className="sidebar bg-dark-blue sm:max-w-[250px] sm:h-4/5 m-4 rounded-xl">
-			<div className="bg-blue h-3/5 rounded-xl p-6">
-				<Image
-					src={"/images/image-jeremy.png"}
-					alt={"profile"}
-					width={100} 
-					height={100} 
-					className="my-3"
-				/>
-				<p className="text-pale-blue mt-5">Report for</p>
-				<h1 className="text-4xl">Jeremy Robson</h1>
-			</div>
-			<div className="flex-col p-6">
-				<Link href="/daily"><p className="text-desaturated-blue my-3">Daily</p></Link>
-				<Link href="/weekly"><p className="text-desaturated-blue my-3">Weekly</p></Link>
-				<Link href="/monthly"><p className="text-desaturated-blue my-3">Monthly</p></Link>
-			</div>
+	<div className="sidebar bg-dark-blue sm:max-w-[250px] sm:h-4/5 m-4 rounded-xl">
+		<div className="bg-blue h-3/5 rounded-xl p-6">
+			<Image
+			src={"/images/image-jeremy.png"}
+			alt={"profile"}
+			width={100}
+			height={100}
+			className="my-3"
+			/>
+			<p className="text-pale-blue mt-5">Report for</p>
+			<h1 className="text-4xl">Jeremy Robson</h1>
 		</div>
-	)
+		<div className="flex-col p-6">
+			<Link id="daily" href="/daily">
+			<p className={`my-3 ${linkColor("/daily")}`}>
+				Daily
+			</p>
+			</Link>
+			<Link href="/weekly">
+			<p className={`my-3 ${linkColor("/weekly")}`}>Weekly</p>
+			</Link>
+			<Link href="/monthly">
+			<p className={`my-3 ${linkColor("/monthly")}`}>Monthly</p>
+			</Link>
+		</div>
+	</div>
+  );
 }
-
--- app/layout.js
+++ app/layout.js
@@ -1,7 +1,5 @@
-import { Inter } from 'next/font/google'
 import './globals.css'
 import Sidebar from './Sidebar/Sidebar'
-// const inter = Inter({ subsets: ['latin'] })
 import { Rubik } from 'next/font/google'
 
 const rubik = Rubik ({
@@ -17,12 +15,12 @@ export const metadata = {
 export default function RootLayout({ children }) {
 	return (
 		<html lang="en" className={rubik.className}>
-		<body>
-			<main className="page sm:flex sm:flex-row h-screen">
-				<Sidebar />
-				{children}
-			</main>
-		</body>
+			<body>
+				<main className="page sm:flex sm:flex-row h-screen">
+					<Sidebar />
+					{children}
+				</main>
+			</body>
 		</html>
 	)
 }
