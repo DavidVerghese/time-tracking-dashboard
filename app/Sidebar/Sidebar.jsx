19b359dc81560da845755257bdddb1978bf6c805
David Verghese <dj.verghese@gmail.com>
2023-11-11 12:32:07 -0500

    Work on responsitivity

--- app/Sidebar/Sidebar.jsx
+++ app/Sidebar/Sidebar.jsx
@@ -3,7 +3,7 @@ import Image from 'next/image'
 
 export default function Sidebar() {
 	return (
-		<div className="bg-dark-blue sm:w-1/4 h-48 sm:h-4/5 m-4 rounded-xl">
+		<div className="bg-dark-blue sm:max-w-xs sm:h-4/5 m-4 rounded-xl">
 			<div className="bg-blue h-3/5 rounded-xl">
 				<Image
 					src={"/images/image-jeremy.png"}
