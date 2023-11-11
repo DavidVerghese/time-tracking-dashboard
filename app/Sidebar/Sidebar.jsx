bf13888304e23641b4dbbb2bc46f91900407347e
David Verghese <dj.verghese@gmail.com>
2023-11-11 12:25:35 -0500

    use Next Image element

--- app/Sidebar/Sidebar.jsx
+++ app/Sidebar/Sidebar.jsx
@@ -1,10 +1,16 @@
 import Link from 'next/link'
+import Image from 'next/image'
 
 export default function Sidebar() {
 	return (
 		<div className="bg-dark-blue sm:w-1/4 h-48 sm:h-4/5 m-4 rounded-xl">
 			<div className="bg-blue h-3/5 rounded-xl">
-				<img src="/images/image-jeremy.png" alt="profile" />
+				<Image
+					src={"/images/image-jeremy.png"}
+					alt={"profile"}
+					width={200} 
+      				height={200} 
+				/>
 			</div>
 			<div className="flex-col">
 				<Link href="/daily"><p>Daily</p></Link>
