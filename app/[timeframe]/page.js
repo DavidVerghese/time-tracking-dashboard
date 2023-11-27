18cdf263d7edb0f54af0749daa3bcea6289af6b8
davidjverghese <davidv@summitappliance.com>
2023-11-27 10:32:08 -0500

    make web title dynamic

--- app/[timeframe]/page.js
+++ app/[timeframe]/page.js
@@ -1,6 +1,15 @@
 import Grid from "../Grid/Grid";
 import GlobalError from "./error";
 
+export async function generateMetadata({ params }) {
+  // read route params
+  const timeframe = params.timeframe
+  // fetch data
+  return {
+    title: `${timeframe[0].toUpperCase() + timeframe.slice(1)}`
+  }
+}
+
 export default function timeTracker({ params }) {
   const acceptableTimeFrames = ['daily', 'weekly', 'monthly'];
   if (acceptableTimeFrames.includes(params.timeframe)) {
