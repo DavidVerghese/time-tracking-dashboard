13b9acbfb9805ad570a06ba234e40c58b051b9c6
davidjverghese <davidv@summitappliance.com>
2023-12-14 14:39:28 -0500

    add interface to category

--- app/Grid/Grid.jsx
+++ app/Grid/Grid.jsx
@@ -1,7 +1,11 @@
 import Category from "../Category/Category";
 import tasksData from '../data.json'
 
-export default function Grid({timeframe}) {
+// interface GridProps {
+// 	timeframe: string
+// }
+
+export default function Grid({timeframe} ) {
 
 	return (
 		<div className="sm:grid grid-cols-2 lg:grid-cols-3 gap-4 sm:w-3/4 sm:h-4/5 m-4">
